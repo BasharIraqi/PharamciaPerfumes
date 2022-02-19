eefedafd63a481007799f06c05fe8ad407f06d53
BasharIraqi <bashar.oroq@hotmail.com>
2022-02-19 17:23:00 +0200

    the last

--- PharamaciaPerfumes.UI/UserControls/EmployeesUC.cs
+++ PharamaciaPerfumes.UI/UserControls/EmployeesUC.cs
@@ -1,15 +1,4 @@
-﻿using PharamaciaPerfumes.UI.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace PharamaciaPerfumes.UI
+﻿namespace PharamaciaPerfumes.UI
 {
     public partial class EmployeesUC : UserControl
     {
